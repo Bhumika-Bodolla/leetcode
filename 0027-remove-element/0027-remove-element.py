b86d8a6b0075b16949c7b45831af7a93c92a5d04
Bhumika-Bodolla <bhumi7shetty@gmail.com>
2025-05-10 09:32:20 +0530

    Time: 3 ms (8.41%), Space: 12.5 MB (51.06%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.py
@@ -0,0 +1,8 @@
+class Solution:
+    def removeElement(self, nums, val):
+        index = 0
+        for i in nums:
+            if i != val:
+                nums[index] = i
+                index += 1
+        return index
